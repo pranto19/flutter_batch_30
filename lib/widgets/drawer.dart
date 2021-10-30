617866afdd453f32ab8b12b5634b0d9f74bcb346
Md. Rakib Uddin <r.rony9966@gmail.com>
2021-10-30 17:14:05 +0600

    day9 complete

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -9,21 +9,53 @@ class MyDrawer extends StatelessWidget {
         "https://media-exp1.licdn.com/dms/image/C5603AQHE4kcpmsYM1g/profile-displayphoto-shrink_200_200/0/1628695083136?e=1637798400&v=beta&t=s80pzsgaBwng1JhjlEoNuE_6rdDLPUAfOfxIcsKrDpc";
 
     return Drawer(
-      child: ListView(
+      child: Column(
         children: [
-          DrawerHeader(
-            padding: EdgeInsets.zero,
-            child: UserAccountsDrawerHeader(
-              margin: EdgeInsets.zero,
-              accountName: Text("Md. Rakib Uddin"),
-              accountEmail: Text("r.rony9966@gmail.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(imageurl),
-              ),
-            ),
+          Container(
+            width: 320,
+            height: 200,
+            child: DrawerHeader(
+                margin: EdgeInsets.all(0),
+                padding: EdgeInsets.all(0),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(top: 20),
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(imageurl),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Name: Rakib Uddin Rony",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Text("Email: r.rony9966@gmail.com"),
+                  ],
+                )),
           ),
         ],
       ),
     );
   }
 }
+
+
+
+      // child: ListView(
+      //   children: [
+      //     DrawerHeader(
+      //       padding: EdgeInsets.zero,
+      //       child: UserAccountsDrawerHeader(
+      //         margin: EdgeInsets.zero,
+      //         accountName: Text("Md. Rakib Uddin"),
+      //         accountEmail: Text("r.rony9966@gmail.com"),
+      //         currentAccountPicture: CircleAvatar(
+      //           backgroundImage: NetworkImage(imageurl),
+      //         ),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+    
